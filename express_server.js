5f88ee05afd0244b92e3a8fd91ce1f441f276dc3
Frank Zhou <frank__zhou@hotmail.com>
2018-06-12 21:10:06 +0000

    added partials and headers

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+app.set("view engine", "ejs");
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -11,14 +15,33 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {eq.params.id
   res.end("<html><body>Hello <b>World</b></body></html>\n");
-})
+})8080
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+  
+}
